9349849e5338696a9d2d617925356204390a1b77
umayal1997 <30542043+umayal1997@users.noreply.github.com>
2017-08-14 15:59:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //move the image
 var element = getElementById('madi');
-madi.onclick=function(){
-  madi.style.marginLeft='200px';  
+element.onclick=function(){
+  element.style.marginLeft='200px';  
 };
