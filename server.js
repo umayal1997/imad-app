1733f970dbb610589d331efb1812b35905fa7026
umayal1997 <30542043+umayal1997@users.noreply.github.com>
2017-08-14 15:33:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('article-two', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
